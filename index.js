762308397d863ef6ffa5f2243e48e2199ebdc645
Tim Leach <timleach635@gmail.com>
2022-09-23 10:39:07 +0100

    Display farm growth state
    Display the two farms as divs containing text, which updates in real time to show the growth time of the farm

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 const socket = io("http://localhost:1995");
 
+const farmsDiv = document.getElementById("farms");
+const crops = [];
+const farms = [];
+
+let hasStoredWorldState = false;
+
 socket.on("harvest", ({ farmId, cropId }) => {
   console.debug(`Harvesting farm ${farmId} containing crop ${cropId}`);
+});
+
+socket.on("world update", (worldState) => {
+  if (!hasStoredWorldState) {
+    worldState.crops.forEach(crop => {
+      crops.push(crop);
+    });
+    worldState.farms.forEach(farm => {
+      const farmDiv = document.createElement("div");
+      farmDiv.classList.add("farm");
+      farmDiv.id = `farm-${farm.id}`;
+      farmDiv.innerText = farm.growthTime;
+      farmsDiv.appendChild(farmDiv);
+      farms.push({
+        ...farm,
+        htmlElement: farmDiv,
+      });
+    });
+
+    hasStoredWorldState = true;
+  } else {
+    worldState.farms.forEach(farm => {
+      const localFarm = farms.find(f => f.id === farm.id);
+      localFarm.htmlElement.innerText = farm.growthTime;
+    });
+  }
 });
