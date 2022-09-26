4dd58762ab00c4d3a6cdb5018cf07c4866a66841
Tim Leach <timleach635@gmail.com>
2022-09-26 18:14:31 +0100

    Implement Phaser farms
    Display the farms and current growth time in Phaser

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,45 @@ import Phaser from "phaser";
 
 const socket = io("http://localhost:1995");
 
-const farmsDiv = document.getElementById("farms");
 const crops = [];
 const farms = [];
 
 let hasStoredWorldState = false;
 
-function preload ()
-{
-  this.load.setBaseURL('http://labs.phaser.io');
-
-  this.load.image('sky', 'assets/skies/space3.png');
-}
-
 function create ()
 {
-  this.add.image(400, 300, 'sky');
+  socket.on("harvest", ({ farmId, cropId }) => {
+    console.debug(`Harvesting farm ${farmId} containing crop ${cropId}`);
+  });
+
+  socket.on("world update", (worldState) => {
+    if (!hasStoredWorldState) {
+      let xValue = 200;
+
+      worldState.crops.forEach(crop => {
+        crops.push(crop);
+      });
+      worldState.farms.forEach(farm => {
+        const farmObject = this.add.rectangle(xValue, 200, 100, 100, 0x00dd00);
+        const growthDisplay = this.add.text(xValue - 50, 100, farm.growthTime.toFixed(2));
+
+        farms.push({
+          ...farm,
+          gameObject: farmObject,
+          growthDisplayObject: growthDisplay,
+        });
+
+        xValue += 150;
+      });
+
+      hasStoredWorldState = true;
+    } else {
+      worldState.farms.forEach(farm => {
+        const localFarm = farms.find(f => f.id === farm.id);
+        localFarm.growthDisplayObject.text = farm.growthTime.toFixed(2);
+      });
+    }
+  });
 }
 
 const config = {
@@ -26,39 +49,8 @@ const config = {
   width: 800,
   height: 600,
   scene: {
-    preload: preload,
     create: create
   }
 };
 
 const game = new Phaser.Game(config);
-
-socket.on("harvest", ({ farmId, cropId }) => {
-  console.debug(`Harvesting farm ${farmId} containing crop ${cropId}`);
-});
-
-socket.on("world update", (worldState) => {
-  if (!hasStoredWorldState) {
-    worldState.crops.forEach(crop => {
-      crops.push(crop);
-    });
-    worldState.farms.forEach(farm => {
-      const farmDiv = document.createElement("div");
-      farmDiv.classList.add("farm");
-      farmDiv.id = `farm-${farm.id}`;
-      farmDiv.innerText = farm.growthTime;
-      farmsDiv.appendChild(farmDiv);
-      farms.push({
-        ...farm,
-        htmlElement: farmDiv,
-      });
-    });
-
-    hasStoredWorldState = true;
-  } else {
-    worldState.farms.forEach(farm => {
-      const localFarm = farms.find(f => f.id === farm.id);
-      localFarm.htmlElement.innerText = farm.growthTime;
-    });
-  }
-});
