3510945a0f97734350bec489442c4c8c22eb3731
Tim Leach <timleach635@gmail.com>
2022-09-28 17:49:05 +0100

    Implement farm areas
    Update the frontend to take advantage of the new farm area information

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 import Phaser from "phaser";
+import Rectangle = Phaser.GameObjects.Rectangle;
+import Text = Phaser.GameObjects.Text;
 
 const socket = io("http://localhost:1995");
 
@@ -15,30 +17,53 @@ function create ()
   });
 
   socket.on("world update", (worldState) => {
-    if (!hasStoredWorldState) {
-      let xValue = 200;
+    const xOffset = 200;
+    const yOffset = 200;
 
+    const getGrowthDisplayText = (farm): string => {
+      return `${farm.growthTime.toFixed(1)} / ${farm.crop.growthTime.toFixed(1)}`;
+    }
+
+    if (!hasStoredWorldState) {
       worldState.crops.forEach(crop => {
         crops.push(crop);
       });
       worldState.farms.forEach(farm => {
-        const farmObject = this.add.rectangle(xValue, 200, 100, 100, 0x00dd00);
-        const growthDisplay = this.add.text(xValue - 50, 100, farm.growthTime.toFixed(2));
+        const farmObject: Rectangle = this.add.rectangle(
+          farm.area.bottomLeft.x + xOffset,
+          farm.area.bottomLeft.y + yOffset,
+          farm.area.size.x,
+          farm.area.size.y,
+          0x00dd00
+        );
+
+        // Set origin to bottom left
+        farmObject.setOrigin(0, 1);
+
+        const growthDisplay: Text = this.add.text(
+          farm.area.bottomLeft.x + xOffset,
+          farm.area.bottomLeft.y + yOffset,
+          getGrowthDisplayText(farm),
+        );
+
+        growthDisplay.setOrigin(0, 1);
+        growthDisplay.setPadding(5, 5, 5, 5);
+        growthDisplay.setColor("#000");
+        growthDisplay.setFontFamily("Tahoma, sans-serif");
+        growthDisplay.setFontSize(20);
 
         farms.push({
           ...farm,
           gameObject: farmObject,
           growthDisplayObject: growthDisplay,
         });
-
-        xValue += 150;
       });
 
       hasStoredWorldState = true;
     } else {
       worldState.farms.forEach(farm => {
         const localFarm = farms.find(f => f.id === farm.id);
-        localFarm.growthDisplayObject.text = farm.growthTime.toFixed(2);
+        localFarm.growthDisplayObject.text = getGrowthDisplayText(farm);
       });
     }
   });
